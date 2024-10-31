621acba48cd2d0f5abae20703ba41e3659e730bc
MaxPacheco02 <a01552369@tec.mx>
2024-10-31 08:19:54 -0600

    new aitsmc implementation, second order sliding surface has a problem

---
+++ src/control/AITSMC_NEW.cpp
@@ -0,0 +1,181 @@
+#include "AITSMC_NEW.h"
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+AITSMC_NEW::AITSMC_NEW(const AITSMCNEWParams &params) {
+  this->p = params;
+  g_u = (1 / (model.m - model.X_u_dot));
+  g_psi = (1 / (model.Iz - model.N_r_dot));
+}
+
+AITSMCNEWParams AITSMC_NEW::defaultParams() {
+  AITSMCNEWParams p;
+  p.k_u = 0.1;
+  p.k_psi = 0.1;
+  p.epsilon_u = 0.1;
+  p.epsilon_psi = 0.1;
+  p.k_alpha_u = 0.5;
+  p.k_alpha_psi = 0.5;
+  p.k_beta_u = 0.5;
+  p.k_beta_psi = 0.5;
+  p.tc_u = 2.0;
+  p.tc_psi = 2;
+  p.q_u = 3.0;
+  p.q_psi = 3.0;
+  p.p_u = 5.0;
+  p.p_psi = 5.0;
+  p.adaptive = 1.0;
+  return p;
+}
+
+double AITSMC_NEW::normalize_angle(double angle_in){
+  double angle_out = std::fmod(angle_in + M_PI, 2 * M_PI);
+  if(angle_out < 0){
+    angle_out += 2*M_PI;
+  }
+  return angle_out - M_PI;
+}
+
+double AITSMC_NEW::angle_dist(double ang1, double ang2){
+  double diff = ang1 - ang2;
+  return normalize_angle(diff);
+}
+
+ControllerOutput AITSMC_NEW::update(const vanttec::ControllerState &s, const AITSMCNEWSetpoint &setpoint) {
+  double Xu = -25;
+  double Xuu = 0;
+  if (std::abs(s.u) > 1.2) {
+    Xu = 64.55;
+    Xuu = -70.92;
+  }
+
+  double Nr = (-0.52) * std::hypot(s.u, s.v);
+  double f_u = (((m - model.Y_v_dot) * s.v * s.r + (Xuu * std::abs(s.u) * s.u + Xu * s.u)) / (m - model.X_u_dot));
+  double f_psi = (((-model.X_u_dot + model.Y_v_dot) * s.u * s.v + (model.Nrr * std::abs(s.r) * s.r + Nr * s.r)) /
+                (Iz - model.N_r_dot));
+
+  double e_u = setpoint.u - s.u;
+  double e_psi = angle_dist(setpoint.psi, s.psi);
+
+  double sign_u = copysign(e_u != 0 ? 1 : 0, e_u);
+  double sign_psi = copysign(e_psi != 0 ? 1 : 0, e_psi);
+
+  double eidot_u = sign_u * std::pow(std::abs(e_u), p.q_u / p.p_u);
+  double eidot_psi = sign_psi * std::pow(std::abs(e_psi), p.q_psi / p.p_psi);
+  ei_u = integral_step * (eidot_u + eidot_u_last) / 2.0 + ei_u;
+  eidot_u_last = eidot_u;
+  ei_psi = integral_step * (eidot_psi + eidot_psi_last) / 2.0 + ei_psi;
+  eidot_psi_last = eidot_psi;
+
+  double edot_psi = ((3./2) * e_psi - 2. * e_psi_last + (1./2) * e_psi_last_last) / (integral_step);
+  // PENDIENTE QUITAR ESE XD
+  // edot_psi = 0;
+  e_psi_last_last = e_psi_last;
+  e_psi_last = e_psi;
+  // std::cout << "ei_psi: " << ei_psi 
+  // // << ", edot_psi: " << edot_psi 
+  // << std::endl;
+
+  // TODO find how to initialize vars
+  static int starting = 1;
+  if(starting == 0){
+    double e_u0 = e_u;
+    double e_psi0 = e_psi;
+    alpha_u = (std::pow(std::abs(e_u0),1-p.q_u/p.p_u))/(p.tc_u*(1-p.q_u/p.p_u));
+    alpha_psi = (std::pow(std::abs(e_psi0),1-p.q_psi/p.p_psi))/(p.tc_psi*(1-p.q_psi/p.p_psi));
+    alpha_u = 0.0001;
+    alpha_psi = 0.1;
+    beta_psi = 0.9;
+    ei_u = -e_u0 / alpha_u;
+    ei_psi = -e_psi0 / alpha_psi;
+    if(alpha_u < 0.0001) ei_u = 0;
+    // if(alpha_psi < 0.0001) ei_psi = 0;
+    starting = 2;
+    std::cout << "huh" << ei_u << std::endl;
+    std::cout << "huhh" << alpha_psi << std::endl;
+  }
+
+  double s_u = e_u + alpha_u * ei_u;
+  double s_psi = edot_psi + beta_psi * e_psi + alpha_psi * ei_psi;
+  // std::cout << "s_psi -> " << s_psi << "\ne_psi: " << e_psi 
+  //   << "\nei_psi: " << ei_psi << "\nedot_psi: " << edot_psi 
+  //   << "\nalpha_psi: " << alpha_psi
+  //   << std::endl;
+  std::cout << "alpha_psi -> " << alpha_psi
+    << "\nalpha_u -> " << alpha_u
+    << std::endl;
+
+  double Ka_dot_u{0}, Ka_dot_psi{0};
+  if(p.adaptive != 0){
+    Ka_dot_u = std::sqrt(p.k_alpha_u) * std::sqrt(std::fabs(s_u)) 
+      - std::sqrt(p.k_beta_u) * std::pow(Ka_u, 2);
+    Ka_dot_psi = std::sqrt(p.k_alpha_psi) * std::sqrt(std::fabs(s_psi)) 
+      - std::sqrt(p.k_beta_psi) * std::pow(Ka_psi, 2);
+    // Update Integrals
+    Ka_u = integral_step * (Ka_dot_u + Ka_dot_last_u) / 2 + Ka_u;
+    Ka_dot_last_u = Ka_dot_u;
+
+    Ka_psi = integral_step * (Ka_dot_psi + Ka_dot_last_psi) / 2 + Ka_psi;
+    Ka_dot_last_psi = Ka_dot_psi;
+  }
+
+
+  double sign_s_u = std::copysign(s_u == 0 ? 0 : 1, s_u);
+  double ua_u = (-Ka_u * std::sqrt(std::abs(s_u)) * sign_s_u) - (Ka_u * p.epsilon_u *  s_u);
+  // double ua_u = (-Ka_u * std::sqrt(std::abs(s_u)) * sign_s_u) - (Ka_u * p.epsilon_u *  std::pow(std::abs(s_u), 0.5) * sign_s_u);
+
+  double sign_s_psi = std::copysign(s_psi == 0 ? 0 : 1, s_psi);
+  double ua_psi = (-Ka_psi * std::sqrt(std::abs(s_psi)) * sign_s_psi) - (Ka_psi * p.epsilon_psi * s_psi);
+
+  double Tx = (setpoint.dot_u + (alpha_u * eidot_u) - f_u - ua_u) / g_u;
+  double Tz = (setpoint.dot_r + (beta_psi * edot_psi) + (alpha_psi * eidot_psi) - f_psi - ua_psi) / g_psi;
+
+  // Clamp forces
+  Tx = std::clamp(Tx, -60.0, 73.0);
+  Tz = std::clamp(Tz, -14.0, 14.0);
+
+  if(starting == 1){
+    Tx = 0;
+    Tz = 0;
+    Ka_u = p.k_u;
+    Ka_dot_last_u = 0;
+
+    Ka_psi = p.k_psi;
+    Ka_dot_last_psi = 0;
+
+    ei_u = -e_u / alpha_u;
+    eidot_u_last = 0;
+
+    ei_psi = -e_psi / alpha_psi;
+    eidot_psi_last = 0;
+    edot_psi = 0;
+    e_psi_last = 0;
+    starting = 0;
+  }
+
+  double port_t = (Tx / 2.0) + (Tz / B);
+  double starboard_t = (Tx / (2.0 * c)) - (Tz / (B * c));
+
+  port_t = std::clamp(port_t, -30.0, 36.5);
+  starboard_t = std::clamp(starboard_t, -30.0, 36.5);
+
+  ControllerOutput out{};
+  out.left_thruster= port_t;
+  out.right_thruster = starboard_t;
+
+  out.Tx = Tx;
+  out.Tz = Tz;
+
+  debugData.e_u = e_u;
+  debugData.e_psi = e_psi;
+  debugData.edot_psi = edot_psi;
+  debugData.s_u = s_u;
+  debugData.s_psi = s_psi;
+  debugData.Ka_u = Ka_u;
+  debugData.Ka_psi = Ka_psi;
+  debugData.Tx = Tx;
+  debugData.Tz = Tz;
+
+  return out;
+}
---
+++ src/control/AITSMC_NEW.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "datatypes.h"
+
+using namespace vanttec;
+
+struct AITSMCNEWParams {
+  double k_u, k_psi,
+        epsilon_u,
+        epsilon_psi,
+        k_alpha_u,
+        k_alpha_psi,
+        k_beta_u,
+        k_beta_psi,
+        tc_u,
+        tc_psi,
+        q_u,
+        q_psi,
+        p_u,
+        p_psi,
+        adaptive;
+};
+
+struct AITSMCNEWSetpoint{
+  double u{0}, psi{0};
+  double dot_u{0}, dot_r{0};
+};
+
+struct AITSMCNEWDebugData {
+  double e_u{0}, e_psi{0};
+  double edot_psi{0};
+  double s_u{0}, s_psi{0};
+  double Ka_u{0}, Ka_psi{0};
+  double Tx{0}, Tz{0};
+};
+
+class AITSMC_NEW {
+public:
+  explicit AITSMC_NEW(const AITSMCNEWParams &params);
+
+  ControllerOutput update(const ControllerState &s, const AITSMCNEWSetpoint &setpoint);
+
+  static AITSMCNEWParams defaultParams();
+
+  double normalize_angle(double angle_in);
+  double angle_dist(double ang1, double ang2);
+
+  [[nodiscard]] AITSMCNEWDebugData getDebugData() const {
+    return debugData;
+  }
+
+private:
+  AITSMCNEWParams p;
+
+  double m = 30; // Mass
+  double Iz = 4.1;
+  double B = 0.41;
+  double c = 0.78;
+
+  static constexpr double integral_step{0.01};
+  double eidot_u_last{0}, eidot_psi_last{0};
+  double e_psi_last{0}, e_psi_last_last{0};
+
+  double alpha_u{0}, alpha_psi{0};
+  double beta_psi{0}, gamma_psi{0};
+  double Ka_u{0}, Ka_psi{0};
+  double ei_u{0}, ei_psi{0};
+
+  double Ka_dot_last_u{0}, Ka_dot_last_psi{0};
+
+  AITSMCNEWDebugData debugData;
+
+  USVModel model = USVModel::getBarcolomeo(); // TODO make this configurable
+  double g_u{0}, g_psi{0};
+};
